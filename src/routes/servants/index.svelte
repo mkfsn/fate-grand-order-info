a8240b248e4ee037269c4233258111363917b122
Pei-Ming Wu <p408865@gmail.com>
2020-09-23 16:58:52 +0800

    feat: create servants page

---
+++ src/routes/servants/index.svelte
@@ -0,0 +1,34 @@
+<script context="module">
+	export function preload({ params, query }) {
+		return this.fetch(`/data/servants.json`).then(r => r.json()).then(servants => {
+			return { servants };
+		});
+	}
+</script>
+
+<script>
+	export let servants;
+</script>
+
+<style>
+	table {
+		margin: 0 0 1em 0;
+		line-height: 1.5;
+	}
+</style>
+
+<svelte:head>
+	<title>Servants</title>
+</svelte:head>
+
+<h1>Servants</h1>
+
+<table>
+	{#each servants as servant}
+	    <tr>
+			<td>{servant.ID}</td>
+			<td><img src={servant.Icon.Src}  alt={servant.Name.EN}/></td>
+			<td>{servant.Name.JP}</td>
+		</tr>
+	{/each}
+</table>
