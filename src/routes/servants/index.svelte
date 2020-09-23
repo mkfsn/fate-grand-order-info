740d0587832a004bf5837e2bce37ae14a084bb2f
Pei-Ming Wu <p408865@gmail.com>
2020-09-23 22:06:59 +0800

    feat: show 編號,圖示,角色名稱,職階,星等,基本ATK,最大ATK,基本HP,最大HP

--- src/routes/servants/index.svelte
+++ src/routes/servants/index.svelte
@@ -24,11 +24,32 @@
 <h1>Servants</h1>
 
 <table>
-	{#each servants as servant}
-	    <tr>
-			<td>{servant.ID}</td>
-			<td><img src={servant.Icon.Src}  alt={servant.Name.EN}/></td>
-			<td>{servant.Name.JP}</td>
+    <thead>
+		<tr>
+			<th>編號</th>
+			<th>圖示</th>
+			<th>角色名稱</th>
+			<th>職階</th>
+			<th>星等</th>
+			<th>基本ATK</th>
+			<th>最大ATK</th>
+			<th>基本HP</th>
+			<th>最大HP</th>
 		</tr>
-	{/each}
+	</thead>
+	<tbody>
+		{#each servants as servant}
+		    <tr>
+				<td>{servant.id}</td>
+				<td><img src={servant.icon['src-1x']}  alt={servant.name.en} srcset="{servant.icon['src-1.5x']} 1.5x, {servant.icon['src-2x']} 2x"/></td>
+				<td>{servant.name.jp}</td>
+                <td><img src={servant.class['src-1x']} width="40" height="40" alt={servant.class.title} srcset="{servant.class['src-1.5x']} 1.5x, {servant.class['src-2x']} 2x"/></td>
+				<td>{servant.rarity}</td>
+				<td>{servant.attack.min}</td>
+				<td>{servant.attack.max}</td>
+				<td>{servant.hp.min}</td>
+				<td>{servant.hp.max}</td>
+			</tr>
+		{/each}
+	</tbody>
 </table>
